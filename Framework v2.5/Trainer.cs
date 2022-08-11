8ec87c49a7995025f3a8029b824d388f9e2f6964
Radu Ciobanu <67599940+RaduTM-spec@users.noreply.github.com>
2022-08-11 09:23:07 +0300

    Add files via upload

--- Framework v2.5/Trainer.cs
+++ Framework v2.5/Trainer.cs
@@ -6,11 +6,11 @@ public class Trainer : TrainerBase
 {
     protected override void Awake()
     {
-
+        base.Awake();
     }
     protected override void Start()
     {
-
+        base.Start();
     }
     protected override void SetupTeam()
     {
