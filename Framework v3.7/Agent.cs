56fdd9e7fff183f53d197dcfc70cbb50cc75e52b
Radu Ciobanu <67599940+RaduTM-spec@users.noreply.github.com>
2022-08-19 13:50:04 +0300

    Add files via upload

---
+++ Framework v3.7/Agent.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using MLFramework;
+public class Agent : AgentBase
+{
+    protected override void Update()
+    {
+        base.Update();
+    }
+    protected override void Manual()
+    {
+        //Implement a keyboard input for your AI - test only
+    }
+    protected override void CollectObservations(ref float[] SensorBuffer)
+    {
+        //Fullfill SensorBuffer with observations
+    }
+    protected override void OnActionReceived(in float[] ActionBuffer)
+    {
+        //ActionBuffer outputs are in [-1f,1f] range
+    }
+
+    //Usefull Methods, use them in CollisionCollider2D, CollisionTrigger2D, Update(), etc. 
+    //AddReward()
+    //SetReward()
+    //EndAction()
+    //GetFitness()
+   
+}
