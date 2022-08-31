3d078e2694ad201ae8504fe895aae3efe1834b23
Radu Ciobanu <67599940+RaduTM-spec@users.noreply.github.com>
2022-08-31 18:19:19 +0300

    Add files via upload
    Heuristic Training data management is changed from the previous version. In 5.0, the user gives the agent a training model and immediately after the constructed training batch is processed for X number of epochs. In the newer version a heuristic module is added. The user now chooses between appending training data or use data from a file. This helps to save training samples and create a larger batch size.

---
+++ Framework v5.3/Trainer.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using MLFramework;
+public class Trainer : TrainerBase
+{
+    [Space, Header("===== Other =====")]
+    public GameObject goal;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+    protected override void Start()
+    {
+        base.Start();
+    }
+    protected override void SetupTeam()
+    {
+        base.SetupTeam();
+        //Add different colors for your AI's
+        //Color add is automatic for agents with SpriteRenderer on the parent gameobject
+    }
+    protected override void EnvironmentAction()
+    {
+        //Add actions for your environment, this method is called in Update - Tip: use Time.deltaTime
+    }
+    protected override void OnEpisodeBegin()
+    {
+        //Actions after Reseting Episode - Example: Activate reward flags/ Modify the environment randomly
+    }
+    protected override void OnEpisodeEnd(ref AI ai)
+    {
+        //Modify each AI before Reseting Episode - Ex: Add postActions reward
+        // ai.agent - used to access the agent gameobject
+        // ai.script - used to acces Agent Script 
+        // ai.fitness - used to get it's current fitness
+
+        //To add reward even if Ai's action ended, use ai.script.AddFitness(value,true) [reward will be added even if AI behaviour becomes Static]
+    }
+}
